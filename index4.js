b909986fa25fe78163be84b24b0d0bd3c81d3d9a
rashidsohail92 <rashidsohail018@gmail.com>
2024-06-07 02:14:45 +0500

    javascript file for login access

---
+++ index4.js
@@ -0,0 +1,11 @@
+let username = prompt("Enter your username: ");
+ let password = prompt("Enter your password: ");
+ if (username === "rashid" || password === "9192") { 
+     alert("Login successful!");
+ } 
+ else if(username === "sohail" || password === "1234" ){
+    alert("login successful")
+ }
+ else {
+     alert("Invalid username or password.");
+ }
