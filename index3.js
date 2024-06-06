787180b22e32b5cadd95c0919fad212e181b4069
rashidsohail92 <rashidsohail018@gmail.com>
2024-06-07 01:51:17 +0500

    i added the 2 files

---
+++ index3.js
@@ -0,0 +1,13 @@
+let age = +prompt("enter your age")
+if (age >=0 && age <=12 ){
+    alert("your are child ")
+}
+else if (age >=12 && age <=30 ){
+    alert("you are young")
+}
+else if (age >=35 && age <=55  ){
+    alert("you are adult ")
+} 
+else if ( age >=60){
+    alert("you are old")
+}
